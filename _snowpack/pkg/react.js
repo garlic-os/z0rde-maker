193fb1795cb45cc62c3690c1c80ce1bc946e565d
the-garlic-os <14845347+the-garlic-os@users.noreply.github.com>
2021-05-27 03:44:22 +0000

    Deploying to gh-pages from @ the-garlic-os/z0rde-maker@52b2ebe5a9925ee8b29f116210cc383958a2a2c1 🚀

--- _snowpack/pkg/react.js
+++
@@ -1,9 +0,0 @@
-import './common/_commonjsHelpers-eb5a497e.js';
-import { r as react } from './common/index-d0e3fe20.js';
-export { r as default } from './common/index-d0e3fe20.js';
-
-
-
-var useEffect = react.useEffect;
-var useState = react.useState;
-export { useEffect, useState };
